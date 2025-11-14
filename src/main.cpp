a166d76d4cd23a2e1e847562e4a0f2749bba1205
David Žižlavský <zizlavsky_david@seznam.cz>
2025-11-15 00:57:20 +0100

    Index buffering

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,13 @@ int main(int argc, char* argv[])
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		-0.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		1, 3, 2
 	};
 
 	unsigned int vao = 0;
@@ -51,6 +57,11 @@ int main(int argc, char* argv[])
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	unsigned int ibo = 0;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	std::string vertexShaderSourceString = readFileContent("shader/shader.vert");
 	char* vertexShaderSource = vertexShaderSourceString.data();
@@ -75,7 +86,7 @@ int main(int argc, char* argv[])
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
